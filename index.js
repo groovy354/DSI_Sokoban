c2c1433867c5feeb5aa689f92f5003e5e8fe4756
Kuba Orlik <groovy354@gmail.com>
2015-05-08 21:10:00 +0200

    Added tests to see which parameter (board_object.js:101) yields the best results

--- index.js
+++ index.js
@@ -6,24 +6,37 @@ var BoardObject = require("./lib/board_object");
 var ConsoleDisplay = require("./lib/display-console.js");
 var Materials = require("./lib/materials/materials.js");
 
-var plansza = new Board(8);
+var plansza = new Board(20);
 
-var amount_of_walls = 10;
+console.log("generating walls...");
+var amount_of_walls = 85;
 for(var i=1; i<=amount_of_walls; i++){
 	var field = plansza.get_random_field();
 	field.set_material(Materials.Stone);
 }
+console.log("done!".green);
 
+console.log("placing crate...");
 var amount_of_crates = 1;
 for(var i=1; i<=amount_of_crates; i++){
-	var field = plansza.get_random_field();
-	if(!field.is_obstacle()){
-		var skrzynka = new BoardObject("crate", String.fromCharCode(9632), "yellow");
-		field.insert(skrzynka);		
-	}
+	var inserted = false;
+	do{
+		var field = plansza.get_random_field();
+		if(!field.is_obstacle()){
+			var skrzynka = new BoardObject("crate", String.fromCharCode(9632), "yellow");
+			field.insert(skrzynka);		
+			inserted = true;
+		}		
+	}while(!inserted);
 }
+console.log("done!".green);
 
-var goal_field = plansza.get_random_field();
+console.log("choosing crate goal...");
+do{
+	var goal_field = plansza.get_random_field();	
+}while(goal_field.is_obstacle());
+console.log("done!".green);
+
 plansza.set_goal(goal_field.position.x, goal_field.position.y);
 
 
@@ -31,6 +44,7 @@ var agent = new BoardObject("agent", String.fromCharCode(9632), "green", 5, 5);
 
 plansza.get_random_field().insert(agent);
 
+/*
 keypress(process.stdin);
 
 process.stdin.on('keypress', function (ch, key) {
@@ -50,17 +64,19 @@ process.stdin.on('keypress', function (ch, key) {
 			agent.move(1, 0);
 			break;
 		}		
-	///*
+	//
 	}catch(error){
 		//console.error(error, error.stack);
 		//throw error;
 	}
-	//*/
+	//
 	if (key && key.ctrl && key.name == 'c') {
 		process.stdin.pause();
 	}
 });
-
+process.stdin.setRawMode(true);
+process.stdin.resume();
+*/
 var initial_board_state = plansza.get_state();
 
 var preview = new ConsoleDisplay(plansza);
@@ -77,14 +93,12 @@ function perform_agent_action(action_path){
 		agent.move(action.y, action.x);
 		setTimeout(function(){
 			perform_agent_action(action_path);
-		}, 300);
+		}, 150);
 	}
 }
 
 
 var display = new ConsoleDisplay(plansza);
 
-perform_agent_action(action_path);
+if(process.argv[2]==undefined) perform_agent_action(action_path);
 
-process.stdin.setRawMode(true);
-process.stdin.resume();
